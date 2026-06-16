cce41ccc3bdaa9473559b299d4ee443cd8616bc1
hmoa575 <hmoa575@outlook.com>
2026-06-16 20:14:08 +0200

    feat: add DniCalculator class and DniCalculatorTest

---
+++ Tasca_S01_E04_Testing/src/main/java/DniCalculator/DniCalculator.java
@@ -0,0 +1,26 @@
+package DniCalculator;
+
+public class DniCalculator {
+    private final String LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+    public char calculateLetter(String id) {
+        validateId(id);
+        int IdNumber = Integer.parseInt(id);
+        return LETTERS.charAt(IdNumber % 23);
+    }
+
+    private void validateId(String id) {
+        if (id == null) {
+            throw new IllegalArgumentException("DNI cannot be null.");
+        }
+
+        if (id.length() != 8) {
+            throw new IllegalArgumentException("DNI must be 8 digits.");
+        }
+
+        if (!id.matches("\\d{8}")) {
+            throw new IllegalArgumentException("DNI must be only numbers 0 - 9.");
+        }
+
+    }
+}
---
+++ Tasca_S01_E04_Testing/src/main/java/DniCalculator/Main.java
@@ -0,0 +1,12 @@
+package DniCalculator;
+
+public class Main {
+    public static void main(String[] args){
+        DniCalculator idCalculator = new DniCalculator();
+
+        String IdUser = "654A2586";
+        char letter = idCalculator.calculateLetter(IdUser);
+
+        System.out.println("The DNI is: " + IdUser + letter );
+    }
+}
